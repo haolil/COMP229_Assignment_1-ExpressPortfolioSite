aa3c5215587081f178bb75df8d078d099842bf66
haolil <lin_rong2005@hotmail.com>
2023-02-14 17:44:17 -0500

    web

---
+++ public/Scripts/app.js
@@ -0,0 +1,20 @@
+/* app.js
+   Name: Shiaw-Rong Lin
+   Student ID: 301293061
+   Date: 13/02/2023
+*/
+
+
+
+(function () {
+    function start() {
+        console.log("App Started...");
+    }
+    window.addEventListener("load", start);
+})();
+
+// Script for the Alert message afte rsubmitting webform in conatact page 
+
+function alertMessage() {
+    alert( "You will be redirected to Home page");
+}
